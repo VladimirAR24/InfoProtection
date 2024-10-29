a2fd8eb901e463b824c5dde5151900c0c5cb4490
VladimirAR24 <raekvladimir@gmail.com>
2024-10-29 18:51:28 +0300

    Обновил бд

--- Migrations/20241029155018_UpdateEncAndUser.Designer.cs
+++ Migrations/20241029155018_UpdateEncAndUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using InfoProtection.Servises;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace InfoProtection.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241029155018_UpdateEncAndUser")]
+    partial class UpdateEncAndUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
